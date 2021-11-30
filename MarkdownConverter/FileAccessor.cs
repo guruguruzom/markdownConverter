3f2926d8296ed5870abae95a232ffbd890a2d70f
Eom <yj.um@lifesemantics.kr>
2021-11-30 11:25:55 +0900

    first commit

---
+++ MarkdownConverter/FileAccessor.cs
@@ -0,0 +1,79 @@
+﻿using LitJson;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace MarkdownConverter
+{
+    class FileAccessor
+    {
+        static StreamWriter output = null;
+        static Queue<Action> msgQueue = new Queue<Action>();
+
+        private readonly string masterIp = @"MasterIP.json";
+        private readonly string path = @"output.txt";
+        private static FileAccessor _instance;
+
+        public static FileAccessor Instance()
+        {
+            if (_instance == null)
+            {
+                _instance = new FileAccessor();
+                
+            }
+            return _instance;
+        }
+
+        public string ReadJsonConfig()
+        {
+            string str = File.ReadAllText(masterIp);
+
+            return str;
+        }
+
+        public void SetJsonConfig(JsonData jsonData)
+        {
+            using (StreamWriter output = new StreamWriter(masterIp, false))
+            {
+                output.WriteLine(jsonData.ToJson());
+            }
+        }
+
+        public void WriteJson(string oneline)
+        {
+            using (output = new StreamWriter(path, true))
+            {
+                output.WriteLine(oneline);
+            }
+        }
+
+        static Thread textLogThread;
+
+        private Action action;
+        private void TextThread()
+        {
+            try
+            {
+                using (output = new StreamWriter(path, true))
+                {
+                    while (msgQueue.Count != 0)
+                    {
+                        action = msgQueue.Dequeue();
+                        action?.Invoke();
+                    }
+                }
+            }
+            finally
+            {
+                //  output.Dispose();
+            }
+            //textLogThread.Abort();
+            textLogThread = null;
+        }
+
+    }
+}
---
+++ MarkdownConverter/MainWindow.xaml.cs
@@ -0,0 +1,128 @@
+﻿using LitJson;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MarkdownConverter
+{
+    /// <summary>
+    /// MainWindow.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private FileAccessor fileAccessor;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            fileAccessor = FileAccessor.Instance();
+
+            
+
+        }
+
+        public void covert(JsonData datas, int depth)
+        {
+
+            if (datas == null)
+                return;
+            if (datas.GetJsonType() == JsonType.Array) {
+                covert(datas[0], depth);
+                return;
+            }
+
+            try
+            {
+                foreach (JsonData data in datas.Keys)
+                {
+                    if (datas[data.ToString()].GetJsonType() == JsonType.Int || datas[data.ToString()].GetJsonType() == JsonType.String)
+                    {
+                        noneKeyNonC(data, datas, depth);
+                    }
+                    else
+                    {
+                        noneKeyNonC(data, datas, depth);
+                        //noneKey(data, depth);
+                        covert(datas[data.ToString()], depth + 1);
+                    }
+
+                }
+            }
+            catch {
+                return;
+            }
+        }
+
+        public void noneKeyNonC(JsonData data, JsonData origin, int depth)
+        {
+            string oneline = "| 모델 |";
+            for (int i = 0; i < depth; i++)
+            {
+                oneline += "┕";
+            }
+            string oper = origin[data.ToString()].GetJsonType().ToString();
+            if (oper.Contains("Int")) {
+                oper = "Number";
+            }
+            oneline += data.ToString() + "|" + oper + "||\n";
+
+            result.AppendText(oneline);
+            //fileAccessor.WriteJson(oneline);
+        }
+
+        public void noneKey(JsonData datas, int depth) {
+            string oneline = "| 모델 |";
+            for (int i = 0; i < depth; i++)
+            {
+                oneline += "┕";
+            }
+
+            string oper = datas.GetJsonType().ToString();
+            if (oper.Contains("Int"))
+            {
+                oper = "Number";
+            }
+
+            oneline += datas.ToString() + "|" + oper + "||\n";
+
+            result.AppendText(oneline);
+            //fileAccessor.WriteJson(oneline);
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string jsonStr = before.Text;
+            //            string jsonStr = fileAccessor.ReadJsonConfig();
+            try
+            {
+                JsonData datas = JsonMapper.ToObject(jsonStr);
+
+                result.Clear();
+                //result.AppendText("| 항목 | 타입 | 설명 | 필수 여부 |\n");
+                //result.AppendText("| --- | --- | --- | :---: |\n");
+                result.AppendText("| 구분 | 항목 | 타입 | 설명 | \n");
+                result.AppendText("| --- | --- | --- | --- | \n");
+                covert(datas, 0);
+            }
+            catch {
+                MessageBox.Show("형식에 맞지않는 json또는 value값에 null이 포함되어 있습니다.");
+            }
+        }
+    }
+}
